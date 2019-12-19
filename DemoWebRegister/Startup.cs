aa18c5c886a286230cf0c172f47c83f8b436eb00
Abhijeet <abhijeet.r@dheya.com>
2019-12-19 15:16:54 +0530

    Demo Commit

---
+++ DemoWebRegister/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoWebRegister.Startup))]
+namespace DemoWebRegister
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
